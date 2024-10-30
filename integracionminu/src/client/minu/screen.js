4bb2f691db1401c4d00142308c24975e44265859
andreslopezuni <andres.lopez.m@uniminuto.edu>
2024-10-30 14:52:38 -0500

    aek de minu

---
+++ integracionminu/src/client/minu/screen.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { request } from "@ombiel/aek-lib";
+
+
+export default Screen = () => {
+  const [tokenUser, setTokenUser] = useState();
+  useEffect(() => {
+    // make a request cmAuth
+    reqz
+
+  }, [])
+
+  return (
+    <>
+      {
+        tokenUser != null ?
+          <div className="pistion-relative">
+            <div className="position-absolute top-50 start-50 translate-middle">
+              <div className="card shadow rounded">
+                <div className="card-body">
+                  <div className="d-flex align-items-center">
+                    <strong className="me-2">Cargando tu chat con Minu...</strong>
+                    <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          :
+          <iframe
+            style={{ height: '100%', width: '100%' }}
+            src={`https://minutest.uniminuto.edu/chat/minu/${tokenUser.mail}`}
+          >
+          </iframe>
+      }
+    </>
+  );
+
+}
